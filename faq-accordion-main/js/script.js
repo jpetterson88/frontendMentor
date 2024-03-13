6c5130353d8b387e19ca7f86df43bc8352e8344f
jpetterson88 <jpmaverick.rj@gmail.com>
2024-03-13 03:07:21 -0300

    updates for faq project

--- faq-accordion-main/js/script.js
+++ faq-accordion-main/js/script.js
@@ -1,2 +1,11 @@
 const question = document.querySelectorAll("h2");
-console.log(question);
+const text = document.querySelectorAll("p");
+const image = document.querySelectorAll("svg");
+console.log(image);
+
+question.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    const toggleText = text[index].classList.toggle("newText");
+    text[index].style.display = toggleText ? "inline" : "none";
+  });
+});
