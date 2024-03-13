2933702652db67dfd151114ffa9bf2070531311d
jpetterson88 <jpmaverick.rj@gmail.com>
2024-03-13 19:28:18 -0300

    updates in javascript for faq project

--- faq-accordion-main/js/script.js
+++ faq-accordion-main/js/script.js
@@ -1,11 +1,15 @@
 const question = document.querySelectorAll("h2");
 const text = document.querySelectorAll("p");
-const image = document.querySelectorAll("svg");
-console.log(image);
+const image = document.querySelectorAll("img");
 
 question.forEach((item, index) => {
   item.addEventListener("click", () => {
     const toggleText = text[index].classList.toggle("newText");
     text[index].style.display = toggleText ? "inline" : "none";
+
+    const changeIcon = image[index + 1].classList.toggle("plusIcon");
+    image[index + 1].src = changeIcon
+      ? "./assets/images/icon-plus.svg"
+      : "./assets/images/icon-minus.svg";
   });
 });
